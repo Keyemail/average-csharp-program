4c76d7e3a77ca2d6842b7d72b7811f4303e2bf50
Keyemail <67425124+Keyemail@users.noreply.github.com>
2023-12-08 18:07:37 -0800

    Commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Average;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int userInput;
+        int averageNumber;
+        List<int> userNumbers = [];
+
+        Console.WriteLine("Please enter numbers, type -1 to stop.");
+        
+        while(true)
+        {
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            if(userInput == -1) break;
+
+            userNumbers.Add(userInput);
+        }
+
+        averageNumber = Average(userNumbers);
+
+        Console.Write("The average number is: " + averageNumber);
+    }
+
+    static int Average(List<int> userNumberInput)
+    {
+        int sum = 0;
+        int average;
+
+        for(int i = 0; i < userNumberInput.Count; i++){
+            sum += userNumberInput[i];
+        }
+
+        average = sum / userNumberInput.Count;
+
+        return average;
+    }
+}
